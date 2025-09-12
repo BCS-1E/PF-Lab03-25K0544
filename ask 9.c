02cf2e63c35aa3873534a69958ada4ee77b696be
Syeda Nisa Batool <snisabatool1116@gmail.com>
2025-09-12 20:31:36 +0500

    Create ask 9.c

---
+++ ask 9.c
@@ -0,0 +1,18 @@
+Task 9
+#include <stdio.h>
+
+int main() {
+   float num1;
+   printf("enter num1: ");
+   scanf("%f", &num1);
+   printf("%.3f\n", num1);
+   printf("%.6f", num1);
+   
+    
+
+   
+   
+   return 0;
+   
+   
+}
