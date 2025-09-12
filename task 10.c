340387be999d8edd2caf2f677eb3c0500ab7fce1
Syeda Nisa Batool <snisabatool1116@gmail.com>
2025-09-12 20:32:08 +0500

    Create task 10.c

---
+++ task 10.c
@@ -0,0 +1,15 @@
+Task 10
+#include <stdio.h>
+
+int main() {
+   printf("Full name:Syeda Nisa Batool\n");
+   printf("FAST NU ID: 25k0544\n");
+   printf("\tTabbed line");
+   printf("\nNew line");
+
+   
+   
+   return 0;
+   
+   
+}
